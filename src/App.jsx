81488db118f9897170574dfee7f1aa6ecdec51f2
sohailexe <div.sohailfaiz@gmail.com>
2024-12-10 16:57:26 +0500

    header added

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import Header from "./Sections/Header.jsx";
+const App = () => {
+    return (
+        <main className="overflow-hidden">
+            <Header />
+        </main>
+    )
+}
+export default App
