6246de488e89e6bdb4dbfcd790eb8ef0a00ace0b
sohailexe <div.sohailfaiz@gmail.com>
2025-02-02 16:36:53 +0500

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import Header from "./Sections/Header.jsx";
 import Hero from "./Sections/Hero.jsx";
 import Features from "./Sections/Features.jsx";
 import Pricing from "./Sections/Pricing.jsx";
+import Faq from "./Sections/Faq.jsx";
+import Testimonials from "./Sections/Testimonials.jsx";
+import Download from "./components/Downlode.jsx";
+import Footer from "./Sections/Footer.jsx";
 
 const App = () => {
   return (
@@ -10,6 +14,10 @@ const App = () => {
       <Hero />
       <Features />
       <Pricing />
+      <Faq />
+      <Testimonials />
+      <Download />
+      <Footer />
     </main>
   );
 };
